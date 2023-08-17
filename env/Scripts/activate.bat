71b99b6ca1ac2a207c935af0605e312515833605
mdsajjadhossain25 <md.sajjad.hossain.025@gmail.com>
2023-08-18 00:10:56 +0600

    rest framework import problem fixed

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\FULL STACK DEVELOPMENT\DJANGO\Discord\env
+set VIRTUAL_ENV=C:\Users\mdsaj\OneDrive\Desktop\33\WEB E\DiscordApp\env
 
 if not defined PROMPT set PROMPT=$P$G
 
