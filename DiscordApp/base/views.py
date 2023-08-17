bf34dd52cd7cad16569438afdfccbc3702ed8b46
mdsajjadhossain25 <md.sajjad.hossain.025@gmail.com>
2023-08-17 22:21:50 +0600

    Mobile responsiveness fixed. New page of browse topic and activity added

--- DiscordApp/base/views.py
+++ DiscordApp/base/views.py
@@ -71,7 +71,7 @@ def home(request):
         Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:4]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -187,4 +187,15 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
     context = {'form':form}
     return render(request, 'update-user.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'topics.html', {'topics':topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages':room_messages}
+    return render(request, 'activity.html', context)
     
