76a6c9d0413c06168a22f1e4f6aa7904e40f9379
mdsajjadhossain25 <md.sajjad.hossain.025@gmail.com>
2023-08-17 23:53:36 +0600

    started working with rest api for this project

--- DiscordApp/DiscordApp/settings.py
+++ DiscordApp/DiscordApp/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'base.apps.BaseConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
