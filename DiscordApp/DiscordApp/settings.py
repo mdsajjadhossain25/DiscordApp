ad4a44987c27fc391c5b1c94ba7477649697aa6c
mdsajjadhossain25 <md.sajjad.hossain.025@gmail.com>
2023-08-19 03:26:03 +0600

    custom user model integrated and profile picture upload system is added

--- DiscordApp/DiscordApp/settings.py
+++ DiscordApp/DiscordApp/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,8 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
